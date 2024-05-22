249df0fa9e84b3de869669018b0974c3470148f8
lezernack <luks.ezck@gmail.com>
2024-05-22 15:45:33 -0600

    changed useEffect code

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import "./App.css";
 const App = () => {
   const APP_ID = "ea7e7fd3";
   const APP_KEY = "3794c105773177f9d3c3189c7c94e59e";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    console.log("Effect has been run");
-  });
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button
-          onClick={() => setCounter(counter + 1)}
-          className="search-button"
-          type="submit"
-        >
-          {counter}
+        <button className="search-button" type="submit">
+          Search
         </button>
       </form>
     </div>
